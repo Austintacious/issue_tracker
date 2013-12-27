704990543d75de080bb7f1bd42f6baf663e623e8
Austin Winslow <awinslow21@gmail.com>
2013-12-27 12:28:19 -0500

    remove unneccessary spec files

---
+++ spec/features/user_creates_an_issue_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+feature "User creates an issue", %q{
+  As a User
+  I want to create an issue
+  So that I can track the progress of the issue
+} do
+
+  # Acceptance Criteria:
+  # * I must provide a title
+  # * I must provide description
+  # * I can mark the issue as reproducible
+  # * I can specify the category of the issue
+
+  context "with valid attributes" do
+    it 'creates an issue with valid attributes' do
+      visit '/issues/new'
+      fill_in "Title", with: "Broken Link"
+      fill_in "Description", with: "The sign in link is broken"
+      check "Is reproducible"
+      fill_in "Category", with: "bug"
+      click_on "Create Issue"
+      expect(page).to have_content("Issue was successfully created")
+    end
+  end
+
+  context "with invalid attributes" do
+    it 'sees errors for invalid attributes' do
+      visit '/issues/new'
+      click_on "Create Issue"
+      expect(page).to have_content("Title can't be blank")
+      expect(page).to have_content("Description can't be blank")
+    end
+  end
+  
+end
