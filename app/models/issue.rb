0d72f19a65848bdc068d2d6a83866d427eb58182
Austin Winslow <awinslow21@gmail.com>
2013-12-28 12:43:25 -0500

    remove unneccessary class method and associated tests

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,10 +3,6 @@ class Issue < ActiveRecord::Base
   belongs_to :category,
     inverse_of: :issues
 
-  def self.categories
-    ["Bug", "Feature Request", "Customer Service"]
-  end
-
   def self.severities
     ["Low", "Moderate", "High"]
   end
--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -1,19 +1,6 @@
 require 'spec_helper'
 
 describe Issue do
-  describe '.categories' do
-    it 'includes Bug' do
-      expect(Issue.categories).to include('Bug')
-    end
-
-    it 'includes Feature Request' do
-      expect(Issue.categories).to include('Feature Request')
-    end
-
-    it 'includes Customer Service' do
-      expect(Issue.categories).to include('Customer Service')
-    end        
-  end
 
   describe '.severities' do
     it 'includes Low' do
